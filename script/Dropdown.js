d11404c2f5b734b46ef8e908f64de073d2c526db
Bryan McClain <bamcclain10@gmail.com>
2017-07-27 20:07:23 -0400

    Added filter dropdown

---
+++ script/Dropdown.js
@@ -0,0 +1,17 @@
+//Handles events for the dropdown menu
+
+jQuery(function ($) {
+        var checkList = $('.dropdown-check-list');
+        checkList.on('click', 'span.anchor', function(event){
+            var element = $(this).parent();
+
+            if ( element.hasClass('visible') )
+            {
+                element.removeClass('visible');
+            }
+            else
+            {
+                element.addClass('visible');
+            }
+        });
+    });
