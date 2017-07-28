6ca7957366525cd88b147a508860584b1cdc38c8
Bryan McClain <bamcclain10@gmail.com>
2017-07-27 20:21:06 -0400

    Javascript parser for filter dropdown

--- script/Password.js
+++ script/Password.js
@@ -1,6 +1,6 @@
 //Not much to it...
-function GeneratePassword(password,account,key) {
-	return Hexacrypt_Encrypt(sha256(account+":"+password+":"+key),key);
+function GeneratePassword(password,account,key,filter) {
+	return Hexacrypt_Encrypt(sha256(account+":"+password+":"+key),key,filter);
 }
 
 //Here is the jQuery
@@ -8,11 +8,21 @@ $(document).ready(function() {
 
 	//Generate the password when da button be clicked
 	$("#Generate").click(function() {
+
+		//Parse the allowed characters
+		var filter = "";
+		$("input[id='filter']").each(function(index,element) {
+			if (!element.checked) {filter+=element.value;}
+		});
+		console.log(filter);	
+
+		//Set the output box
 		$(".output").val(
 			GeneratePassword(
 				$(".password").val(),
 				$(".account").val(),
-				$(".secret").val()
+				$(".secret").val(),
+				filter
 		).substring(0,48));
 	});
 
@@ -23,5 +33,16 @@ $(document).ready(function() {
 		$(".secret").val("");
 		$(".output").val("");
 	});
+
+	//Check all checkboxes
+	$("#SelectAll").click(function() {
+		$("input[id='filter']").prop('checked', true);
+	});
+
+	//Uncheck all checkboxes
+	$("#DeselectAll").click(function() {
+		$("input[id='filter']").prop('checked', false);
+	});
+
 
 });
